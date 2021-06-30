f5119275d87035fe4e7fc08c83d187c615e6d009
Hans Petter Selasky <hps@selasky.org>
2021-06-30 20:34:29 +0200

    hpsat_generate: Correct verbose printout.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- hpsat_generate.cpp
+++ hpsat_generate.cpp
@@ -1667,7 +1667,7 @@ top:
 	}
 
 	for (size_t z = 0; z != maxvar; z++)
-		outcnf("c Solution in (" << a.z[z].v << " / " << b.z[z].v << ") = " << f.z[z].v << "\n");
+		outcnf("c Solution in (" << a.z[z].v << " * " << b.z[z].v << ") = " << f.z[z].v << "\n");
 
 	do_cnf_header();
 
